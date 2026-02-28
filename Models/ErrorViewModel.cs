8fde55e4b7619e7bbe3e54e370c5ee3bf2313876
MauroTi <mauro4879@gmail.com>
2026-02-28 18:42:00 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClientesPedidos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
